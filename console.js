3830e9ad5b4e507146131f8d2247541df5bf4274
kirre02 <74262686+kirre02@users.noreply.github.com>
2024-12-02 14:23:44 +0100

    finished with all the required querys

--- console.js
+++ console.js
@@ -1,5 +1,12 @@
 use moviedb
 
+//Creating a  unqiue index
+db.movies.createIndex(
+    {title: 1},
+    {unique: true}
+)
+
+// Inserting some movies for the database
 db.movies.insertMany([
     {
       title: "Jurassic World: Fallen Kingdom",
@@ -23,6 +30,7 @@ db.movies.insertMany([
     }
 ])
 
+// inserting two more movies here
 db.movies.insertOne({
       title: "Tag",
       genres: [ "Comedy", "Action" ],
@@ -45,28 +53,83 @@ db.movies.insertOne({
       type: "movie"
 })
 
+//check if unique index works
+db.movies.insertOne({
+      title: "Titanic",
+      genres: [ "Romance", "Thriller" ],
+      runtime: 194,
+      rated: "PG-13",
+      year: 1998,
+      directors: [ "James Cameron" ],
+      cast: [ "Leonardo DiCaprio", "Kate Winslet" ],
+      type: "movie"
+})
+
+//deleting movie where Chris Pratt is included
 db.movies.deleteOne( { cast: "Chris Pratt" } )
 
+//getting all the movies that have been added
+db.movies.find()
+
+//find the database by title
+db.movies.find({title: "Tag"})
+
+//adding a new field to every movie that have been add
 db.movies.updateMany(
     {},
     { $set: { IMDB_score: 0 }}
 )
 
+//updating the newly created field so that the movies have scores
+db.movies.bulkWrite([
+    {
+        updateOne: {
+            filter: {title: "Jurassic World: Fallen Kingdom"},
+            update: {$set: {IMDB_score: 65}}
+        }
+    },
+      {
+        updateOne: {
+            filter: {title: "Titanic"},
+            update: {$set: {IMDB_score: 70}}
+        }
+    },
+      {
+        updateOne: {
+            filter: {title: "Tag"},
+            update: {$set: {IMDB_score: 50}}
+        }
+    },
+      {
+        updateOne: {
+            filter: {title: "Star Wars: the empire strikes back"},
+            update: {$set: {IMDB_score: 80}}
+        }
+    },
+])
+
+// Updating the score field for the movie Titanic
 db.movies.updateOne({title: "Titanic"}, {$set: {IMDB_score: 75}})
 
+//search by number and sort them ascending
+db.movies.find({IMDB_score: {$gt: 50}}, {},{sort: {IMDB_score: 1}})
+
+// counting how many documents that contains the rating of PG and PG-13
+db.movies.countDocuments({rated: {$in: ["PG", "PG-13"]}})
+
+//find a movie and return the directors and cast field
+db.movies.find({title: "Star Wars: the empire strikes back"}, {directors: 1, cast: 1})
+
+//Returns all the titles and IMDB_scores for each movie.
+db.movies.find({}, {title: 1, IMDB_score: 1})
+
+
 db.movies.updateOne(
-    {title: ""},
-    {$push: {cast: [""]}
-})
+    {title: "Titanic"},
+    {$push: {cast: "Billy Zane"}}
+ )
 
-db.movies.find()
-
-db.movies.find(
-    {rated: {$in: ["PG", "PG-13"]}}
-)
-
-db.movies.find({title: "Titanic"})
+db.movies.find({title:"Titanic"})
 
 
-
-
+db.dropDatabase()
